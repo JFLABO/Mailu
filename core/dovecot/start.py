401c973e200a64ae04593a67602c2dc82ce52aab
Martin Ashby <martin@ashbysoft.com>
2018-03-20 17:04:19 +0000

    Tidied up start.py for dovecot. Added explicit !SSLv3 param to dovecot.conf so that it actually starts up

--- core/dovecot/start.py
+++ core/dovecot/start.py
@@ -5,15 +5,27 @@ import os
 import socket
 import glob
 
-convert = lambda src, dst: open(dst, "w").write(jinja2.Template(open(src).read()).render(**os.environ))
-
 # Actual startup script
 os.environ["FRONT_ADDRESS"] = socket.gethostbyname("front")
 if os.environ["WEBMAIL"] != "none":
+    try:
 	os.environ["WEBMAIL_ADDRESS"] = socket.gethostbyname("webmail")
+    except:
+        print("Error resolving webmail host, WEBMAIL_ADDRESS isn't set")
 
-for dovecot_file in glob.glob("/conf/*"):
-    convert(dovecot_file, os.path.join("/etc/dovecot", os.path.basename(dovecot_file)))
+def convert(src, dst):
+    print("Converting file ", src, " to ", dst)
+    with open(dst, "w") as dst_file, open(src, "r") as src_file:
+        template = src_file.read()
+        config = jinja2.Template(template).render(**os.environ)
+        dst_file.write(config)
+
+confroot = "/conf"
+for dirpath, dirs, files in os.walk(confroot):
+    for f in files:
+        filefullpath = os.path.join(dirpath, f)
+        filerelpath = os.path.relpath(filefullpath, confroot)
+        convert(filefullpath, os.path.join("/etc/dovecot", filerelpath))
 
 # Run postfix
 os.system("chown -R mail:mail /mail /var/lib/dovecot")
